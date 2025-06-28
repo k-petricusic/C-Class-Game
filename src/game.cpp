23f99c380b4438434a619de6f31fee7f7fa13742
Kian K <balakiank@gmail.com>
2025-06-27 20:07:04 -0700

    Added loop to playGame() method

--- src/game.cpp
+++ src/game.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
 #include <cstdlib>
+#include "../include/Guard.h"
+#include "../include/Movable.h"
+#include "../include/Player.h"
+#include "../include/Screen.h"
+#include "board.cpp"
+#include "menu_screens.cpp"
+#include "move.cpp"
+
 using namespace std;
 
 void playGame() {
     cout << "Game started! [Simulation of game here]" << endl;
+    while (true) {
+        Screen* current_screen = new Title_Screen();
+        current_screen->show();
+        current_screen->get_user_input(current_screen);
+    }
 }
 
 int main() {
